14654d8765b5b104673cf9d6f81dae005fac9b75
zen_mudd <sergey.pilipchak@gmail.com>
2012-11-12 03:14:17 +0200

    renewed the folder system

---
+++ figures_geo.rb
@@ -0,0 +1,77 @@
+class Cirlcle
+	def initialize
+		
+	end
+	
+	def circumference
+		
+	end
+	
+	def area
+		
+	end
+end
+
+class Ellipse
+	def initialize
+		
+	end
+	
+	def circumference
+		
+	end
+	
+	def area
+		
+	end
+end
+
+class Triangle
+	def initialize
+		
+	end
+	
+	def perimetr
+		
+	end
+	
+	def area
+		
+	end
+end
+
+class Quadrangle
+	def initialize (side_a, side_b, side_c, side_d)
+		@a = side_a
+		@b = side_b
+		@c = side_c
+		@d = side_d
+		
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
